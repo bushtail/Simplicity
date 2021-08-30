ad95973657f23e0b878be98d290fe05316ac2195
enosich <evannosich@icloud.com>
2021-08-30 18:15:55 -0400

    obsidian textures wow

--- src/main/java/ca/evannosich/simplicity/client/SimplicityClient.java
+++ src/main/java/ca/evannosich/simplicity/client/SimplicityClient.java
@@ -1,6 +1,14 @@
 package ca.evannosich.simplicity.client;
 
-@net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
-public class SimplicityClient {
+import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
 
+@Environment(EnvType.CLIENT)
+public class SimplicityClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+
+    }
 }
