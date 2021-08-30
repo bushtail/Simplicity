f39e09eeaa585883920cee66758279f6ba4ae3e7
enosich <evannosich@icloud.com>
2021-08-30 15:40:01 -0400

    well it still doesn't work, all i need to do is find why it isn't

--- src/main/java/ca/evannosich/simplicity/Simplicity.java
+++ src/main/java/ca/evannosich/simplicity/Simplicity.java
@@ -4,29 +4,26 @@ import net.fabricmc.api.ModInitializer;
 import ca.evannosich.simplicity.registry.SimplicityRegistry;
 import net.fabricmc.fabric.api.loot.v1.FabricLootPoolBuilder;
 import net.fabricmc.fabric.api.loot.v1.event.LootTableLoadingCallback;
-import net.fabricmc.fabric.api.resource.ResourceManagerHelper;
-import net.fabricmc.fabric.api.resource.SimpleSynchronousResourceReloadListener;
 import net.minecraft.block.Blocks;
-import net.minecraft.loot.ConstantLootTableRange;
+import net.minecraft.loot.BinomialLootTableRange;
 import net.minecraft.loot.entry.ItemEntry;
 import net.minecraft.util.Identifier;
 
 public class Simplicity implements ModInitializer {
-    public static final Identifier OBSIDIAN_LOOT_TABLE_ID = Blocks.OBSIDIAN.getLootTableId();
+    private static final Identifier OBSIDIAN_LOOT_TABLE_ID = Blocks.OBSIDIAN.getLootTableId();
     public static final String MOD_ID ="simplicity";
     public static final String NAME = "Simplicity";
 
     @Override
     public void onInitialize() {
-
+        SimplicityRegistry.register();
+        LootTableLoadingCallback.EVENT.register((resourceManager, manager, id, supplier, setter) -> {
+            if(OBSIDIAN_LOOT_TABLE_ID.equals(id)) {
+                FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
+                        .rolls(BinomialLootTableRange.create(10, 0.1f))
+                        .with(ItemEntry.builder(SimplicityRegistry.OBSIDIAN_SHARD));
+                supplier.pool(poolBuilder);
+            }
+        });
     }
-
-    LootTableLoadingCallback.EVENT.register((resourceManager, lootManager, id, table, setter) ->{
-        if(OBSIDIAN_LOOT_TABLE_ID.equals(id)) {
-            FabricLootPoolBuilder poolBuilder = FabricLootPoolBuilder.builder()
-                    .rolls(ConstantLootTableRange.create(1))
-                    .with(ItemEntry.builder(SimplicityRegistry.OBSIDIAN_SHARD));
-            table.pool(poolBuilder);
-        }
-    });
 }
--- src/main/java/ca/evannosich/simplicity/client/SimplicityClient.java
+++ src/main/java/ca/evannosich/simplicity/client/SimplicityClient.java
@@ -1,3 +1,6 @@
+package ca.evannosich.simplicity.client;
+
 @net.fabricmc.api.Environment(net.fabricmc.api.EnvType.CLIENT)
 public class SimplicityClient {
+
 }
